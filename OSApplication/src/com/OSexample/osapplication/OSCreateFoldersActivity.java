32646329efc5dd77cb66421e55fb6c4a3653ed31
坂田和輝 <k.sakata246@gmail.com>
2014-06-08 22:00:31 +0900

    マイリストに追加する確認ダイアログ生成

--- OSApplication/src/com/OSexample/osapplication/OSCreateFoldersActivity.java
+++ OSApplication/src/com/OSexample/osapplication/OSCreateFoldersActivity.java
@@ -2,10 +2,94 @@ package com.OSexample.osapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class OSCreateFoldersActivity extends Activity {
-	protected void onCreate(Bundle savedInstanceState) {
+
+	// ContextMenuアイテムid
+	static final int CONTEXT_MENU1_ID = 0;
+	ArrayAdapter<String> adapter;
+
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.imageview);
-	}	
+		setContentView(R.layout.create_new_folder);
+
+		// ListViewオブジェクトの取得
+		ListView listView = (ListView) findViewById(R.id.LISTadd_view);
+
+		// ArrayAdapterオブジェクト生成
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1);
+
+		// Buttonオブジェクト取得
+		Button Listadd_button = (Button) findViewById(R.id.Listadd_button);
+
+		// クリックイベントの通知先指定
+		Listadd_button.setOnClickListener(new OnClickListener() {
+
+			// クリックイベント
+			@Override
+			public void onClick(View v) {
+				// 要素追加
+				addStringData();
+			}
+		});
+		// Adapterのセット
+		listView.setAdapter(adapter);
+		// コンテキストメニュー登録
+		registerForContextMenu(listView);
+
+	}
+
+	// 要素（Stringデータ）追加処理
+	private void addStringData() {
+
+		// EditTextオブジェクト取得
+		EditText edit = (EditText) findViewById(R.id.edit_text_list);
+
+		// EditText(テキスト)を取得し、アダプタに追加
+		adapter.add(edit.getText().toString());
+
+	}
+
+	/*
+	 * コンテキストメニュー生成時処理
+	 */
+	public void onCreateContextMenu(ContextMenu menu, View view,
+			ContextMenuInfo info) {
+		super.onCreateContextMenu(menu, view, info);
+		AdapterContextMenuInfo adapterinfo = (AdapterContextMenuInfo) info;
+		ListView listView = (ListView) view;
+
+		// コンテキストメニューのタイトル
+		menu.setHeaderTitle((String) listView
+				.getItemAtPosition(adapterinfo.position));
+		// メニュー出し
+		menu.add(0, CONTEXT_MENU1_ID, 0, "削除する");
+
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {// ContextMenuアイテム選択イベント
+		switch (item.getItemId()) {
+		case CONTEXT_MENU1_ID:
+			// 削除項目を選択されたテキスト名取得
+			EditText edit = (EditText) findViewById(R.id.edit_text_list);
+			// 取得されたテキストの削除
+			adapter.remove(edit.getText().toString());
+
+			return true;
+		default:
+			return super.onContextItemSelected(item);
+		}
+	}
 }
--- OSApplication/src/com/OSexample/osapplication/OSYoutubePlayActivity.java
+++ OSApplication/src/com/OSexample/osapplication/OSYoutubePlayActivity.java
@@ -1,7 +1,12 @@
 package com.OSexample.osapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
@@ -10,27 +15,33 @@ import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayer.OnInitializedListener;
 import com.google.android.youtube.player.YouTubePlayerView;
 
-
 /**
  * OSYoutubeResultListActivityから遷移し、ここで動画を再生する
  * 
  */
 public class OSYoutubePlayActivity extends YouTubeBaseActivity implements
-		OnInitializedListener {
+		OnInitializedListener, OnClickListener {
 	private static final int RECOVERY_DIALOG_REQUEST = 1;
+	// URLのフィールド
 	private static String url;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Intent intent = getIntent();
+		// Intentで渡されたURLをセット
 		OSYoutubePlayActivity.url = intent.getStringExtra("youtubeURL");
+		// Video IDに変換
 		video_id(url);
 		setContentView(R.layout.youtube_player_fragment);
 
 		// YoutubePlayerViewにDeveloperキーを設定
 		YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
 		youTubeView.initialize(OSYoutubeDeveloperKey.DEVELOPER_KEY, this);
+
+		// btnの実装
+		Button btn = (Button) findViewById(R.id.youtube_add_button);
+		btn.setOnClickListener(this);
 	}
 
 	@Override
@@ -42,9 +53,7 @@ public class OSYoutubePlayActivity extends YouTubeBaseActivity implements
 			errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
 		} else {
 			// エラー回避が不可能な場合、トーストのみを表示
-			String errorMessage = String.format(
-
-			errorReason.toString());
+			String errorMessage = String.format(errorReason.toString());
 			Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
 		}
 	}
@@ -54,11 +63,39 @@ public class OSYoutubePlayActivity extends YouTubeBaseActivity implements
 			YouTubePlayer player, boolean wasRestored) {
 		// YouTubeの動画IDを設定
 		if (!wasRestored) {
-			String video_id =  this.video_id(url);
+			String video_id = this.video_id(url);
 			player.cueVideo(video_id);
 		}
 	}
 
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case (R.id.youtube_add_button):
+			AlertDialog.Builder alertDlg = new AlertDialog.Builder(this);
+			alertDlg.setTitle("My Listへの追加");
+			alertDlg.setMessage("My Listへ追加しますか？");
+			alertDlg.setPositiveButton("OK",
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							Object PlayListInfo = new Object();
+						}
+					});
+			alertDlg.setNegativeButton("Cancel",
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+						}
+					});
+			alertDlg.create().show();
+			break;
+		}
+	}
+
+	/**
+	 * YouTubeのURLでv=以下にあるVideo IDを取得する
+	 * 
+	 */
 	private String video_id(String url) {
 		String video_id = url.substring(url.indexOf("=") + 1);
 		return video_id;
